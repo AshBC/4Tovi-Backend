09cab67ef38eb217c872e9b1c4df026b4bc7300a
Ash Conery <aconery@gmail.com>
2021-05-12 19:00:08 -0700

    updates to routes and server.js

---
+++ routes/siteRoutes.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+const router = express.Router();
+const fs = require("fs");
+const { POINT_CONVERSION_HYBRID } = require("constants");
+
+function loadRecipes() {
+    return fs.readFileSync("./data/recipes.json", "utf8");
+}
+
+router.get("/", (req,res) =>{
+    console.log(req);
+    const RList = JSON.parse(loadRecipes());
+    const RMapped = RList.map((pizzas) => {
+        return {
+            id: pizzas.id,
+            name: pizzas.name,
+            instructions: pizzas.instructions,
+            ingredients: pizzas.ingredients,
+        };
+    });
+    res.json(RMapped);
+});
+
+router.get("/:id", (req, res) => {
+    const RList = JSON.parse(loadRecipes());
+    const foundRecipeIndex = RList.findIndex((pizzas) => {
+      return pizzas.id === req.params.id;
+    });
+    res.json(RList[foundRecipeIndex]);
+  });
+
+router.delete("/:id", (req,res) => {
+    const data = JSON.parse(loadRecipes());
+    const newList = data.filter((item) => item.id !== req.params.id);
+    
+    fs.writeFileSync("./data/recipes.json", JSON.stringify(newList));
+    console.log(newList);
+    res.json(newList);
+});
+
+router.post("/", (req, res) => {
+    if (req.body.name === "" && req.body.description === "") {
+      res.status(422).send("please enter a name and description");
+    } else {
+      const RList = JSON.parse(loadRecipes());
+      const newRecipe = {
+        id: uuidv4(),
+        name: req.body.name,
+        instructions: req.body.channel,
+        ingredients: req.body.description,
+      };
+      RList.push(newRecipe);
+      fs.writeFileSync("./data/recipes.json", JSON.stringify(RList));
+  
+      res.json({
+        message: "posted to recipes endpoint",
+        recipePosted: newRecipe,
+      });
+    }
+  });
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require ('express');
+const app = express ();
+const cors = require ('cors');
+const siteRoutes = require ('./routes/siteRoutes');
+const { restart } = require('nodemon');
+const PORT = 3001;
+
+app.use(cors());
+app.use(express.static('public'));
+app.use(express.json());
+app.use('/recipes', siteRoutes);
+
+app.get ('/', (req,res) =>{
+    res.json({
+        greeting: 'Welcome to Tovi Pizza',
+    });
+});
+app.listen(PORT, console.log(`listening at:http://localhost:${PORT}`));
